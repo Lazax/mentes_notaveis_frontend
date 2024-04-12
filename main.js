e4e3719479a851690ec0d4e795f1976fd19ffa64
Andre Souza <andre.souza@littleboat.com.br>
2024-04-12 15:15:19 -0300

    remoção da inserção automatica de valores padrão

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-document.querySelector('#brand').value = 'Brastemp';
-document.querySelector('#color').value = 'azul';
-document.querySelector('#height').value = 200;
-document.querySelector('#width').value = 300;
-document.querySelector('#length').value = 100;
-document.querySelector('#oven-glass-door-height').value = 150;
-document.querySelector('#oven-glass-door-width').value = 250;
-
 const stoveController = new StoveController();
 
 document.querySelector('#stove-form').addEventListener('click', (event) => {
